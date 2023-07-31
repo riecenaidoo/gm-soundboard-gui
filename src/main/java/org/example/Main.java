37720e4912b4749f881dc4328db6683c148304b2
riecenaidoo <riecenaidoo@outlook.com>
2023-07-31 11:34:58 +0200

    basic JFrame code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//1. Create the frame.
+        JFrame frame = new JFrame("FrameDemo");
+
+//2. Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//3. Create components and put them in the frame.
+//...create emptyLabel...
+        JLabel emptyLabel = new JLabel("Hello World.");
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+//4. Size the frame.
+        frame.pack();
+//... center the frame
+        frame.setLocationRelativeTo(null);
+
+//5. Show it.
+        frame.setVisible(true);
     }
 }
