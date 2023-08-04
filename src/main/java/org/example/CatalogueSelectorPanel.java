75a62c0f3311c41a4db5766ba1bb3293416467e6
Riece Naidoo <125825660+riecenaidoo@users.noreply.github.com>
2023-08-04 13:35:19 +0200

    Dynamically Built Catalogue (#9)
    * representing the Catalogue's tabbed pane in JSON
    * added com.fasterxml.jackson as a dependency for handling JSON

--- src/main/java/org/example/CatalogueSelectorPanel.java
+++ src/main/java/org/example/CatalogueSelectorPanel.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Singleton container class for the Channel Selector portion of the interface.
@@ -10,10 +18,66 @@ public class CatalogueSelectorPanel extends JPanel {
 
     private static CatalogueSelectorPanel INSTANCE;
 
+    JTabbedPane tabbedPane;
+
     private CatalogueSelectorPanel() {
         super();
-        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane = new JTabbedPane();
+        this.add(tabbedPane);
+    }
 
+    public static CatalogueSelectorPanel getCatalogueSelector() {
+        if (INSTANCE == null) INSTANCE = new CatalogueSelectorPanel();
+        return INSTANCE;
+    }
+
+
+    public void loadUI(String filepath) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode arrayNode = mapper.readTree(new File(filepath));
+            if (!arrayNode.isArray())
+                throw new RuntimeException("[WARNING] The structure of the JSON is not as expected!");
+
+            for (JsonNode jsonNode : arrayNode) {
+                JPanel groupPanel = new JPanel();
+                String groupName = jsonNode.get("Group").asText();
+
+                JsonNode itemNode = jsonNode.get("Items");
+                if (!itemNode.isArray())
+                    throw new RuntimeException("[WARNING] The structure of the JSON is not as expected!");
+                for (JsonNode item : itemNode) {
+                    String title = item.get("Title").asText();
+                    JButton button = new JButton(title);
+
+                    JsonNode songArray = item.get("Songs");
+                    if (!songArray.isArray())
+                        throw new RuntimeException("[WARNING] The structure of the JSON is not as expected!");
+                    List<String> songs = new ArrayList<>();
+                    for (JsonNode song : songArray) {
+                        songs.add(song.asText());
+                    }
+
+                    button.addActionListener(e -> System.out.printf("[POST Request] Play these songs: '%s'.\n", songs));
+                    groupPanel.add(button);
+                }
+
+                tabbedPane.add(groupName, groupPanel);
+            }
+
+
+        } catch (JsonProcessingException e) {
+            System.out.printf("[WARNING] There was a problem parsing '%s'.\n", filepath);
+            System.out.printf("\tReason: '%s'.\n", e.getMessage());
+            loadDummyUI();
+        } catch (IOException e) {
+            System.out.printf("[WARNING] There was a problem opening '%s'.\n", filepath);
+            System.out.printf("\tReason: '%s'.\n", e.getMessage());
+            loadDummyUI();
+        }
+    }
+
+    public void loadDummyUI() {
         JPanel gamePanel = new JPanel();
         gamePanel.add(new Button("Game"));
         gamePanel.add(new Button("Game"));
@@ -38,12 +102,5 @@ public class CatalogueSelectorPanel extends JPanel {
         tabbedPane.addTab("COMBAT", combatPanel);
         tabbedPane.addTab("MOOD", moodPanel);
         tabbedPane.addTab("AMBIENCE", ambiencePanel);
-
-        this.add(tabbedPane);
-    }
-
-    public static CatalogueSelectorPanel getCatalogueSelector() {
-        if (INSTANCE == null) INSTANCE = new CatalogueSelectorPanel();
-        return INSTANCE;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.add(CatalogueSelectorPanel.getCatalogueSelector());
+
+        CatalogueSelectorPanel catalogueSelector = CatalogueSelectorPanel.getCatalogueSelector();
+        catalogueSelector.loadUI("src/main/resources/mock_catalogue.json");
+        panel.add(catalogueSelector);
 
         JPanel mediaPanel = new JPanel();
         mediaPanel.add(doMiniplayer());
