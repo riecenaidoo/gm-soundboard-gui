0f00598f49d9e0bbcdd078890e5db9ca9ba94d42
riecenaidoo <riecenaidoo@outlook.com>
2023-08-26 13:46:00 +0200

    simplified build process using Makefile

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import java.util.List;
 
 public class API {
--- src/main/java/MiniplayerPanel.java
+++ src/main/java/MiniplayerPanel.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
