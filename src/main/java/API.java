9218c5722b2aa9ef7d942600a27597a4c79512eb
riecenaidoo <riecenaidoo@outlook.com>
2023-08-26 19:53:33 +0200

    comment out unnecessary print calls
    - NOTE: Logging may be something that could be useful now.

--- src/main/java/API.java
+++ src/main/java/API.java
@@ -18,37 +18,37 @@ public class API {
         String command = stringBuilder.toString();
         System.out.println(command);
 
-        System.out.printf("[POST Request] Play these songs: '%s' -> '%s' \n", songs, command);
+//        System.out.printf("[INFO][POST Request] Play these songs: '%s' -> '%s' \n", songs, command);
         client.send(command);
     }
 
     void join_channel(Object selectedItem) {
-        System.out.printf("[POST Request] Set the Bot's audio channel to <%s>.\n", selectedItem);
+//        System.out.printf("[INFO][POST Request] Set the Bot's audio channel to <%s>.\n", selectedItem);
         client.send("join " + selectedItem);
     }
 
     void set_volume(int volume) {
-        System.out.printf("[POST Request] Set the Bot's audio level '<%d>'.\n", volume);
+//        System.out.printf("[INFO][POST Request] Set the Bot's audio level '<%d>'.\n", volume);
         client.send("volume " + volume);
     }
 
     void resume() {
-        System.out.print("[POST Request] Pause the audio.\n");
+//        System.out.print("[INFO][POST Request] Pause the audio.\n");
         client.send("resume");
     }
 
     void pause() {
-        System.out.print("[POST Request] Resume the audio.\n");
+//        System.out.print("[INFO][POST Request] Resume the audio.\n");
         client.send("pause");
     }
 
     void skip() {
-        System.out.print("[POST Request] Skip to the next song.\n");
+//        System.out.print("[INFO][POST Request] Skip to the next song.\n");
         client.send("skip");
     }
 
     void prev() {
-        System.out.print("[POST Request] Skip to the previous song.\n");
+//        System.out.print("[INFO][POST Request] Skip to the previous song.\n");
         client.send("prev");
     }
 }
