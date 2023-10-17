2ec8484fb44077d50a0b39c9e1369e701ea290fa
riecenaidoo <riecenaidoo@outlook.com>
2023-10-17 14:39:47 +0200

    scratch

--- server/src/main/java/SingleServer.java
+++ server/src/main/java/SingleServer.java
@@ -8,44 +8,63 @@ public class SingleServer {
     private BufferedWriter out;
     private BufferedReader in;
 
-    public SingleServer(int PORT) throws IOException {
-        serverSocket = new ServerSocket(PORT);
+    public SingleServer(int PORT) {
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("[FATAL] Could not establish a SingleServer over Port:%s.\n", PORT));
+        }
     }
 
     public static void main(String[] args) {
         int PORT = 5000;
+        new SingleServer(PORT).run();
+    }
+
+    public void connect() {
         try {
-            new SingleServer(PORT).run();
+            Socket socket = serverSocket.accept();
+
+            OutputStream outputStream = socket.getOutputStream();
+            out = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+            InputStream inputStream = socket.getInputStream();
+            in = new BufferedReader(new InputStreamReader(inputStream));
         } catch (IOException e) {
-            System.out.printf("[FATAL] Could not establish a SingleServer over Port:%s.\n", PORT);
+            throw new RuntimeException(("[WARNING] There was a problem connecting & setting up the socket."));
+        } finally {
+            closeQuietly();
         }
     }
 
-    public void connect() throws IOException {
-        Socket socket = serverSocket.accept();
+    public void run() {
+        connect();
+        try {
+            String clientRequest;
+            do {
+                System.out.println("Awaiting...");
+                clientRequest = in.readLine();
+                System.out.printf("[<---] '%s'.\n", clientRequest);
 
-        OutputStream outputStream = socket.getOutputStream();
-        out = new BufferedWriter(new OutputStreamWriter(outputStream));
-
-        InputStream inputStream = socket.getInputStream();
-        in = new BufferedReader(new InputStreamReader(inputStream));
+                String serverResponse = "200";
+                System.out.printf("[--->] '%s'.\n", serverResponse);
+                out.write(serverResponse);
+                out.flush();
+            } while (clientRequest != null);
+        } catch (IOException ex) {
+            System.out.println("[INFO] Disconnected.");
+        } finally {
+            closeQuietly();
+        }
     }
 
-    public void run() throws IOException {
-
-
+    private void closeQuietly() {
         try {
-            String clientRequest = in.readLine();
-            System.out.printf("[<---] '%s'.\n", clientRequest);
-
-            String serverResponse = "200";
-            System.out.printf("[--->] '%s'.\n", serverResponse);
-            out.write(serverResponse);
-        } catch (IOException ex) {
-
-        } finally {
-            in.close();
-            out.close();
+            if (in != null) in.close();
+            if (out != null) out.close();
+        } catch (IOException e) {
+            System.out.println("[WARNING] There was a problem closing ServerSocket I/O streams.");
+            e.printStackTrace();
         }
     }
 }
