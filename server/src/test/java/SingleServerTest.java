ef7e92987a96116667fde5f2d0efe646ee7a3e50
riecenaidoo <riecenaidoo@outlook.com>
2023-10-19 13:26:30 +0200

    test skeleton

---
+++ server/src/test/java/SingleServerTest.java
@@ -0,0 +1,36 @@
+class SingleServerTest {
+
+    /**
+     * Can connect to the SingleServer
+     * over a local port.
+     */
+    @org.junit.jupiter.api.Test
+    void connect() {
+
+    }
+
+    /**
+     * Can send one string to the server
+     * and receive a response.
+     */
+    @org.junit.jupiter.api.Test
+    void sendReceiveOne() {
+
+    }
+
+    /**
+     * Can send/receive many string messages to the server.
+     */
+    @org.junit.jupiter.api.Test
+    void sendReceiveMany() {
+
+    }
+
+    /**
+     * Can disconnect from the SingleServer and reconnect.
+     */
+    @org.junit.jupiter.api.Test
+    void disconnectReconnect() {
+
+    }
+}
