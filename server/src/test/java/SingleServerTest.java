fdabfa3033daf0c13325582728ea0f98bdb5aa65
riecenaidoo <riecenaidoo@outlook.com>
2023-10-19 13:49:35 +0200

    connection test passing, but need to setup before & afters

--- server/src/test/java/SingleServerTest.java
+++ server/src/test/java/SingleServerTest.java
@@ -1,27 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+/**
+ * @see SingleServer
+ */
 class SingleServerTest {
 
     /**
      * Can connect to the SingleServer
      * over a local port.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void connect() {
-
+        try {
+            Socket socket = new Socket("localhost", 5000);
+            socket.close();
+        } catch (SocketException s) {
+            Assertions.fail("Could not connect.");
+        } catch (UnknownHostException | IllegalArgumentException ex) {
+            throw new RuntimeException("Invalid host/port specification.");
+        } catch (SecurityException | IOException e) {
+            throw new RuntimeException("Check firewall, and local machine resources.");
+        }
     }
 
     /**
      * Can send one string to the server
      * and receive a response.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void sendReceiveOne() {
-
+//        PrintStream out = new PrintStream(socket.getOutputStream(),true);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(
+//                socket.getInputStream()))
     }
 
     /**
      * Can send/receive many string messages to the server.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void sendReceiveMany() {
 
     }
@@ -29,7 +52,7 @@ class SingleServerTest {
     /**
      * Can disconnect from the SingleServer and reconnect.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void disconnectReconnect() {
 
     }
