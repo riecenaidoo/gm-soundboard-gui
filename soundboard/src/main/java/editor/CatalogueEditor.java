6c9cdcaed0c6bf20c25d45c0ecc89a3e6abc62d3
riecenaidoo <riecenaidoo@outlook.com>
2023-11-28 21:04:21 +0200

    add tooltips, labels to Catalogue Editor - added newlines to seperate creation and insertion of different ui elements

--- soundboard/src/main/java/editor/CatalogueEditor.java
+++ soundboard/src/main/java/editor/CatalogueEditor.java
@@ -7,7 +7,8 @@ import javax.swing.*;
 public class CatalogueEditor extends JPanel {
 
     Catalogue catalogue;
-    JComboBox<String> groups;
+    JComboBox<String> groupSelector;
+    JLabel currentGroup;
     JButton addGroup;
     JButton editGroup;
     JButton removeGroup;
@@ -16,17 +17,29 @@ public class CatalogueEditor extends JPanel {
         super();
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.catalogue = catalogue;
-
+        JPanel groupInfoPanel = new JPanel();
+        this.add(groupInfoPanel);
         JPanel groupsPanel = new JPanel();
-        this.add(groupsPanel);
         groupsPanel.setLayout(new BoxLayout(groupsPanel, BoxLayout.LINE_AXIS));
-        groups = new JComboBox<>();
-        groupsPanel.add(groups);
+        this.add(groupsPanel);
+
+        currentGroup = new JLabel("GROUP");
+        groupInfoPanel.add(currentGroup);
+
+        groupSelector = new JComboBox<>();
+        groupSelector.setToolTipText("Select a Group from the Catalogue");
+        groupsPanel.add(groupSelector);
+
         addGroup = new JButton("+");
+        addGroup.setToolTipText("Add a new Group to the Catalogue");
         groupsPanel.add(addGroup);
+
         removeGroup = new JButton("-");
+        removeGroup.setToolTipText("Remove this Group from the Catalogue");
         groupsPanel.add(removeGroup);
+
         editGroup = new JButton("Edit");
+        editGroup.setToolTipText("Edit this Group");
         groupsPanel.add(editGroup);
     }
 }
