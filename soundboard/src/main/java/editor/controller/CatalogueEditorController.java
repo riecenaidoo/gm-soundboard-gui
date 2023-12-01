b822adb26e7b39a02d9a696cc35d42a8b8055b93
riecenaidoo <riecenaidoo@outlook.com>
2023-12-01 12:57:37 +0200

    created Group & Playlist getters
    - Handles logic of retrieving selected elements from combo boxes in views, and retrieving the relevant element in the model.

--- soundboard/src/main/java/editor/controller/CatalogueEditorController.java
+++ soundboard/src/main/java/editor/controller/CatalogueEditorController.java
@@ -5,6 +5,9 @@ import editor.view.GroupsPanel;
 import editor.view.PlaylistsPanel;
 import soundboard.model.catalogue.Catalogue;
 import soundboard.model.catalogue.Group;
+import soundboard.model.catalogue.Playlist;
+
+import java.util.Optional;
 
 /**
  * A CatalogueEditorController handles the input of the user on the editing panels
@@ -30,30 +33,64 @@ public class CatalogueEditorController {
     }
 
     /**
+     * @return Selected Group option from the Catalogue.
+     */
+    private Optional<Group> getSelectedGroup() {
+        GroupsPanel groupsPanel = this.view.getGroupsPanel();
+        int selectedIndex = (groupsPanel.getGroupSelector().getSelectedIndex()) - 1; // See GroupsPanel#view
+        try {
+            return Optional.of(model.get(selectedIndex));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * @return Selected Playlist option from the selected Catalogue Group.
+     */
+    private Optional<Playlist> getSelectedPlaylist() {
+        Optional<Group> selectedGroup = getSelectedGroup();
+        if (selectedGroup.isEmpty()) return Optional.empty();
+
+        Group group = selectedGroup.get();
+        PlaylistsPanel playlistsPanel = this.view.getPlaylistsPanel();
+        int selectedIndex = (playlistsPanel.getPlaylistSelector().getSelectedIndex()) - 1; // See PlaylistsPanel#view
+        try {
+            return Optional.of(group.get(selectedIndex));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+    }
+
+
+    /**
      * Selecting a Group will load its Playlists into the Playlist Panel
      * for editing.
      * <p>
      * Updates the View with the currently selected group in the GroupPanel's group selector.
      */
     public void selectGroup() {
-        GroupsPanel groupsPanel = this.view.getGroupsPanel();
-        int selectedIndex = groupsPanel.getGroupSelector().getSelectedIndex();
-        if (selectedIndex <= 0) {
+        Optional<Group> selectedGroup = getSelectedGroup();
+        if (selectedGroup.isEmpty()) {
             view.groupDeselected();
         } else {
-            view.getPlaylistsPanel().view(model.get(selectedIndex - 1));  // See GroupsPanel#view
+            view.getPlaylistsPanel().view(selectedGroup.get());
             view.groupSelected();
         }
     }
 
+    /**
+     * Selecting a Playlist will load its Songs into the PlaylistEditor Panel
+     * for editing.
+     * <p>
+     * Updates the View with the currently selected group in the PlaylistsPanel's group selector.
+     */
     public void selectPlaylist() {
-        PlaylistsPanel playlistsPanel = this.view.getPlaylistsPanel();
-        int selectedIndex = playlistsPanel.getPlaylistSelector().getSelectedIndex();
-        if (selectedIndex <= 0) {
+        Optional<Playlist> selectedPlaylist = getSelectedPlaylist();
+        if (selectedPlaylist.isEmpty()) {
             view.playlistDeselected();
         } else {
-            Group group = model.get(this.view.getGroupsPanel().getGroupSelector().getSelectedIndex() - 1);
-            view.getPlaylistEditorPanel().view(group.get(selectedIndex - 1)); // See PlaylistsPanel#view
+            view.getPlaylistEditorPanel().view(selectedPlaylist.get());
             view.playlistSelected();
         }
     }
